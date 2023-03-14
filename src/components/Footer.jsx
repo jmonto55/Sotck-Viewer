55e3cd77c1834b0b3ce67192fa58242e36aa3594
jmonto55 <jmonto55@gmail.com>
2023-03-13 23:11:17 -0500

    Add Home layout and styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import github from '../assets/github.svg';
+
+const Footer = () => (
+  <>
+    <footer className="mt-10">
+      <p className="text-lg text-center text-white mb-2">© 2023 Jose Montoya. All rights reserved.</p>
+      <p className="text-lg text-center text-white">Built with 💙 in Medellin, CO</p>
+      <div className="flex justify-evenly">
+        <img src={github} alt="github" className="opacity-10" />
+      </div>
+    </footer>
+  </>
+);
+
+export default Footer;
