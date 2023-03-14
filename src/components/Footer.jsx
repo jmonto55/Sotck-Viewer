9c5a21824125d9686c8065c173708ddb09cdcabf
jmonto55 <jmonto55@gmail.com>
2023-03-13 23:21:54 -0500

    Minor styles changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import github from '../assets/github.svg';
 
 const Footer = () => (
   <>
-    <footer className="mt-10">
+    <footer className="mt-10 opacity-75">
       <p className="text-lg text-center text-white mb-2">© 2023 Jose Montoya. All rights reserved.</p>
       <p className="text-lg text-center text-white">Built with 💙 in Medellin, CO</p>
       <div className="flex justify-evenly">
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,13 +7,13 @@ import tsx from '../assets/tsx.svg';
 import etf from '../assets/etf.svg';
 
 const Home = () => (
-  <div className="z-10 w-screen text-white mt-20 px-10 mb-10 flex flex-col justify-center">
+  <div className="z-10 w-screen text-white mt-20 px-5 mb-10 flex flex-col justify-center">
     <header className="m-auto max-w-screen-2xl bg-white w-full bg-opacity-10 h-48 p-2 md:p-10 rounded-lg flex justify-between items-center">
       <h2 className="animate-pulse-slow text-center md:text-2xl text-white">Welcome to Quantum Stocks</h2>
       <img src={stock} alt="stock" className="animate-pulse-slow mt-10 w-40 md:w-64" />
     </header>
     <section className="m-auto w-full max-w-screen-2xl">
-      <h2 className="mt-10 p-5 bg-black">Stocks by Market</h2>
+      <h2 className="mt-10 p-5 bg-black rounded-t-lg opacity-70">Stocks by Market</h2>
       <div className="grid grid-cols-2">
         <button className="relative p-10 bg-neutral-900 h-40 flex flex-col justify-center items-center hover:opacity-60" type="button">
           <span className="material-symbols-outlined absolute top-4 right-4 opacity-50 hover:scale-110">arrow_circle_right</span>
